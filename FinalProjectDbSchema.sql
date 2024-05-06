b475448b3975017201e7636a6f6d079c8cb767bc
liambaker10 <116182077+liambaker10@users.noreply.github.com>
2024-05-05 20:50:31 -0400

    Update FinalProjectDbSchema.sql

--- FinalProjectDbSchema.sql
+++ FinalProjectDbSchema.sql
@@ -1,35 +1,34 @@
 DROP TABLE song CASCADE CONSTRAINTS;
 CREATE TABLE song (
-  title    VARCHAR2(50) NOT NULL, 
-  artistID NUMBER(4) NOT NULL,
-  songID   NUMBER(4) NOT NULL,
-  releaseDate DATE,
-  genreID     number(4) not null, 
-  PRIMARY KEY (songID),
-  -- FOREIGN KEY (artistID) REFERENCES artist(artistID),
-  -- foreign key (genreID) references genre(genreID)
+  title    varchar2(50) not null,
+  artistID    number(4) not null,
+  songID    number(4) not null,
+  releaseDate      date,
+  genreID    number(4) not null,
+  primary key (songID, title),
+  foreign key (artistID) references artist(artistID),
+  foreign key (genreID) references genre(genreID)
 );
 
 DROP TABLE artist CASCADE CONSTRAINTS;
 CREATE TABLE artist (
-  name     VARCHAR2(50) NOT NULL, 
-  artistID NUMBER(4) NOT NULL,
-  PRIMARY KEY (artistID)
+  name    varchar2(50) not null,
+  artistID    number(4) not null,
+  primary key (artistID)
 );
 
- DROP TABLE album CASCADE CONSTRAINTS;
+DROP TABLE album CASCADE CONSTRAINTS;
 CREATE TABLE album (
-  name     VARCHAR2(50) NOT NULL, 
-  albumID  NUMBER(4) NOT NULL,
-  artistID NUMBER(4) NOT NULL,
-  PRIMARY KEY (albumID),
-  FOREIGN KEY (artistID) REFERENCES artist(artistID)
+  name        varchar2(50) not null,
+  albumID     number(4) not null,
+  artistID    number(4) not null,
+  primary key (albumID),
+  foreign key (artistID) references artist(artistID)
 );
-
 
 DROP TABLE applicationUser CASCADE CONSTRAINTS;
 CREATE TABLE applicationUser (
-  fname    varchar2(15) not null, 
+  fname    varchar2(15) not null,
   lname    varchar2(15) not null,
   userID   number(4) not null,
   username varchar(30) not null,
@@ -44,3 +43,11 @@ CREATE TABLE genre (
   primary key (genreID)
 );
 
+DROP TABLE playlist CASCADE CONSTRAINTS;
+CREATE TABLE playlist (
+  playlistName       varchar2(20) not null,
+  songs      varchar2(500),
+  userID     number(4) not null,
+  primary key (playlistName),
+  foreign key (userID) references applicationUser(userID)
+);
